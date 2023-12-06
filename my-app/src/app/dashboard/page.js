19c230cd8a35cba0b2e1ecdf2d8935a38b663a5a
Marissa Halim <halimmq2@gmail.com>
2023-12-05 14:10:34 -1000

    add image

--- my-app/src/app/dashboard/page.js
+++ my-app/src/app/dashboard/page.js
@@ -34,21 +34,21 @@ const Page = async () => {
         <div className='container pt-4'>
           <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full">
-              <img src="/9MSC-1.JPG" className="w-full flex justify-center items-center" />
+              <img src="/9MSC6.JPG" className="w-full flex justify-center items-center" />
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide5" className="btn btn-circle">❮</a>
                 <a href="#slide2" className="btn btn-circle">❯</a>
               </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-              <img src="/9MSC-2.JPG" className="w-full" />
+              <img src="/9MSC-1.JPG" className="w-full" />
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide1" className="btn btn-circle">❮</a>
                 <a href="#slide3" className="btn btn-circle">❯</a>
               </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-              <img src="/9MSC-3.JPG" className="w-full" />
+              <img src="/9MSC-2.JPG" className="w-full" />
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide2" className="btn btn-circle">❮</a>
                 <a href="#slide4" className="btn btn-circle">❯</a>
